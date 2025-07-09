a5e3a31f31509301f6a804c4ebbf0af209e12b40
Ian89darksword <happyanyanpie999.ian@gmail.com>
2025-07-09 16:17:28 +0800

    class-3

--- pages/hand_book.py
+++ pages/hand_book.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import os
 
+
 folderPath = "markdown"  # 設定資料夾路徑
 files = os.listdir(folderPath)  # 取的資料夾下的所有檔案
 print(files)
 
 files.sort()  # 將清單排序 , 預設是由小到大排序
 
-for f in files:  # ['class1.md', 'class2.md'']逐一讀取所有 ,md檔案
+for f in files:  # ['class1.md', 'class2.md','class3.md']逐一讀取所有 ,md檔案
     # markdown/class2.md
     with open(os.path.join(folderPath, f), "r", encoding="utf-8") as file:
         content = file.read()
